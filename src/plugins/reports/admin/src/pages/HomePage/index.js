c515d53a731a8608f11e838446250b50c3314d72
Alan Neres <alanneres10@hotmail.com>
2022-07-19 22:29:41 -0300

    feat: init project

---
+++ src/plugins/reports/admin/src/pages/HomePage/index.js
@@ -0,0 +1,19 @@
+/*
+ *
+ * HomePage
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+import pluginId from '../../pluginId';
+
+const HomePage = () => {
+  return (
+    <div style={{display: 'flex'}}>
+      <h1 style={{fontSize: "1.4rem"}}>Gráficos</h1>
+    </div>
+  );
+};
+
+export default memo(HomePage);
